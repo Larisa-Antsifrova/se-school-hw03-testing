b686f0bd511decc9de9d1c3e893a62ea30a7e285
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-21 02:18:16 +0300

    Separated rates provider and rates service.

---
+++ rates_providers/coinlayer-provider.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const coinlayer = require('../http/axios-coinlayer');
+
+const { COINLAYER_API_KEY } = process.env;
+
+class CoinlayerProvider {
+  constructor(http, apiKey) {
+    this.http = http;
+    this.apiKey = apiKey;
+  }
+
+  async fetchBtcToUahRate() {
+    try {
+      const {
+        data: { timestamp, target, rates },
+      } = await this.http.get('/live', {
+        params: {
+          access_key: this.apiKey,
+          target: 'UAH',
+          symbols: 'BTC',
+        },
+      });
+
+      return { timestamp, target, rates };
+    } catch (error) {
+      console.log('Error in fetchBtcToUahRate: ', error.message);
+    }
+  }
+}
+
+module.exports = new CoinlayerProvider(coinlayer, COINLAYER_API_KEY);
