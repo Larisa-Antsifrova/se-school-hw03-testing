609cabd4bb081d67be53885d47374b7761439b9e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-21 00:50:10 +0300

    Refactored services to have dependency injections.

--- services/jwt-token-service.js
+++ services/jwt-token-service.js
@@ -1,16 +1,34 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+const ApiError = require('../exceptions/api-errors');
+const { HttpCodes } = require('../helpers/constants');
+
 const { JWT_SECRET_KEY } = process.env;
 
 class TokenService {
-  constructor(secretKey) {
+  constructor({ secretKey, errorHandler }) {
     this.secretKey = secretKey;
+    this.errorHandler = errorHandler;
   }
 
   generateToken(payload) {
     return jwt.sign(payload, this.secretKey, { expiresIn: '4h' });
   }
+
+  verifyToken(token) {
+    try {
+      jwt.verify(token, JWT_SECRET_KEY);
+    } catch (error) {
+      throw new this.errorHandler({
+        status: HttpCodes.UNAUTHORIZED,
+        message: error.message,
+      });
+    }
+  }
 }
 
-module.exports = new TokenService(JWT_SECRET_KEY);
+module.exports = new TokenService({
+  secretKey: JWT_SECRET_KEY,
+  errorHandler: ApiError,
+});
