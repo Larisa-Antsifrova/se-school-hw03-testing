eb2b9442f860df83ce62a1229d97e0e03f009904
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-21 21:02:03 +0300

    Refactored ApiError tests.

--- tests/exeptions/api-errors.test.js
+++ tests/exeptions/api-errors.test.js
@@ -1,39 +1,7 @@
 const ApiError = require('../../exceptions/api-errors');
 const { HttpCodes, Messages } = require('../../helpers/constants');
 
-describe('ApiError: static method conflict', () => {
-  let result;
-
-  beforeAll(() => {
-    result = ApiError.conflict();
-  });
-
-  test('should return conflict status code', () => {
-    expect(result.status).toBe(HttpCodes.CONFLICT);
-  });
-
-  test('should return conflict message', () => {
-    expect(result.message).toBe(Messages.emailConflict);
-  });
-});
-
-describe('ApiError: static method invalidCreds', () => {
-  let result;
-
-  beforeAll(() => {
-    result = ApiError.invalidCreds();
-  });
-
-  test('should return unauthorized status code', () => {
-    expect(result.status).toBe(HttpCodes.UNAUTHORIZED);
-  });
-
-  test('should return invalid creds message', () => {
-    expect(result.message).toBe(Messages.invalidCreds);
-  });
-});
-
-describe('ApiError: called via constructor', () => {
+describe('ApiError:', () => {
   let result;
   const passedStatus = HttpCodes.UNAUTHORIZED;
   const passedMessage = Messages.noJWT;
