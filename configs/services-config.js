5afb3d7167b385371a9851ad05af946a85af84de
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-22 12:33:14 +0300

    Added password service config.

--- configs/services-config.js
+++ configs/services-config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const RatesService = require('../services/rates-service');
 const AuthService = require('../services/auth-service');
@@ -12,22 +13,29 @@ const ApiError = require('../exceptions/api-errors');
 
 const { JWT_SECRET_KEY } = process.env;
 
+const coinlayerRatesService = new RatesService({
+  provider: CoinlayerProvider,
+  errorHandler: ApiError,
+});
+
 const jwtTokenService = new TokenService({
   jwtProvider: jwt,
   secretKey: JWT_SECRET_KEY,
   errorHandler: ApiError,
 });
 
-const coinlayerRatesService = new RatesService({
-  provider: CoinlayerProvider,
-  errorHandler: ApiError,
-});
+const bcryptPasswordService = new PasswordService(bcrypt);
 
 const apiAuthService = new AuthService({
   usersCollection: Users,
-  passwordService: PasswordService,
+  passwordService: bcryptPasswordService,
   tokenService: jwtTokenService,
   errorHandler: ApiError,
 });
 
-module.exports = { coinlayerRatesService, apiAuthService, jwtTokenService };
+module.exports = {
+  coinlayerRatesService,
+  jwtTokenService,
+  bcryptPasswordService,
+  apiAuthService,
+};
