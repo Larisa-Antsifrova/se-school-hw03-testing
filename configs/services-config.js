b36544beb0446a11c4b729add4c5400c3fcc44fd
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-22 11:44:49 +0300

    Rearranged configs.

--- configs/services-config.js
+++ configs/services-config.js
@@ -1,22 +1,33 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 const RatesService = require('../services/rates-service');
+const AuthService = require('../services/auth-service');
+const PasswordService = require('../services/password-service');
+const TokenService = require('../services/jwt-token-service');
+
+const Users = require('../repositories/users-repository');
 const CoinlayerProvider = require('../rates_providers/coinlayer-provider');
 const ApiError = require('../exceptions/api-errors');
+
+const { JWT_SECRET_KEY } = process.env;
+
+const jwtTokenService = new TokenService({
+  jwtProvider: jwt,
+  secretKey: JWT_SECRET_KEY,
+  errorHandler: ApiError,
+});
 
 const coinlayerRatesService = new RatesService({
   provider: CoinlayerProvider,
   errorHandler: ApiError,
 });
 
-const AuthService = require('../services/auth-service');
-const Users = require('../repositories/users-repository');
-const PasswordService = require('../services/password-service');
-const TokenService = require('../services/jwt-token-service');
-
 const apiAuthService = new AuthService({
   usersCollection: Users,
   passwordService: PasswordService,
-  tokenService: TokenService,
+  tokenService: jwtTokenService,
   errorHandler: ApiError,
 });
 
-module.exports = { coinlayerRatesService, apiAuthService };
+module.exports = { coinlayerRatesService, apiAuthService, jwtTokenService };
