71d178b8c64553e938b0adce44243477c721cd25
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-23 22:29:14 +0300

    Edited description.

--- tests/app/home.spec.js
+++ tests/app/home.spec.js
@@ -2,24 +2,26 @@ const request = require('supertest');
 const app = require('../../app');
 const { HttpCodes, Messages } = require('../../helpers/constants');
 
-describe('GET /', () => {
-  let response;
+describe('home endpoint', () => {
+  describe('GET /', () => {
+    let response;
 
-  beforeAll(async () => {
-    response = await request(app).get('/');
-  });
+    beforeAll(async () => {
+      response = await request(app).get('/');
+    });
 
-  it('responds with json', async () => {
-    expect(response.headers['content-type']).toEqual(
-      expect.stringContaining('json'),
-    );
-  });
+    it('responds with json', async () => {
+      expect(response.headers['content-type']).toEqual(
+        expect.stringContaining('json'),
+      );
+    });
 
-  it('responds with 200 status code', async () => {
-    expect(response.statusCode).toBe(HttpCodes.OK);
-  });
+    it('responds with 200 status code', async () => {
+      expect(response.statusCode).toBe(HttpCodes.OK);
+    });
 
-  it('responds with welcome message', async () => {
-    expect(response.body.message).toBe(Messages.welcomeHome);
+    it('responds with welcome message', async () => {
+      expect(response.body.message).toBe(Messages.welcomeHome);
+    });
   });
 });
